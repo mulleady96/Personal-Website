488a79fb3d888e11503ad94d0dabe0562e11952c
mulleady96 <32593466+mulleady96@users.noreply.github.com>
2022-01-25 21:53:56 +0000

    Am   code improvements (#39)
    * CSS Improvements
    
    * Test Git action secret
    
    * test action
    
    * test
    
    * test
    
    * test workflow
    
    * test
    
    * test
    
    * test
    
    * Commit changes
    
    * commit message
    
    * commit messages
    
    * C m
    
    * commit
    
    * m
    
    * Try
    
    * m
    
    * message
    
    * me
    
    * commits
    
    * commits
    
    * commits
    
    * commits
    
    * commits
    
    * test 1
    
    * test 2
    
    * test 3
    
    * test 4
    
    * test 5
    
    * test 6
    
    * Delete credentials.ts
    
    * test 1
    
    * Css button changes
    
    * update branch
    
    * Theme persistence & form saving
    
    * Optimised images with calibre/image-actions
    
    * AM: Change images and css fixes
    
    * AM: css change home component
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,61 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
-import { CountUpOptions } from 'countup.js';
-
+import { Component, OnInit } from "@angular/core";
+import { trigger, transition, style, animate } from "@angular/animations";
+import { CountUpOptions } from "countup.js";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  animations: [ // Slide items up from the bottom of screen.
-        trigger('itemState', [
-            transition('void => *', [
-                style({transform: 'translateY(100%)'}),
-                animate('0.6s ease-in-out')
-            ]),
-            transition('* => void', [
-                animate('0.6s ease-in-out', style({transform: 'translateY(100%)'}))
-            ])
-        ])
-    ]
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
+  animations: [
+    // Slide items up from the bottom of screen.
+    trigger("itemState", [
+      transition("void => *", [
+        style({ transform: "translateY(100%)" }),
+        animate("0.6s ease-in-out"),
+      ]),
+      transition("* => void", [
+        animate("0.6s ease-in-out", style({ transform: "translateY(100%)" })),
+      ]),
+    ]),
+  ],
 })
-
-
 export class HomeComponent implements OnInit {
+  opts: CountUpOptions;
+  showDiv = false;
+  video: string;
 
-    opts: CountUpOptions;
-    showDiv = false;
-    video: string;
-
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
-      this.useOptions();
-      this.randomVideos();
+    this.useOptions();
+    this.randomVideos();
   }
 
   toggleDiv = () => {
-      this.showDiv = !this.showDiv;
-  }
+    this.showDiv = !this.showDiv;
+  };
 
   useOptions = () => {
     this.opts = {
       duration: 6,
-      separator: ','
+      separator: ",",
     };
-  }
+  };
 
   randomVideos = () => {
     const src = [
-      '../assets/12 pins.JPG',
-      '../assets/Inverin.jpg',
-      '../assets/Pier1.jpg',
-      '../assets/lighthouse.jpg',
-      '../assets/Pier2.jpg'
+      "../assets/DJI_0249.JPG",
+      "../assets/DJI_0406.JPG",
+      "../assets/Pier1.jpg",
+      "../assets/LoughInagh.JPG",
+      "../assets/Pier2.jpg",
     ];
     // Every page load, renders a random video.
     this.video = src[Math.floor(Math.random() * src.length)];
     return this.video;
-  }
+  };
 }
