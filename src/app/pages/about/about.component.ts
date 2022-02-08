9ce6ffa8a30a2a40f9f4d4a3c4f7d97d7b255cf3
Andrew Mulleady <andrewmulleady@live.ie>
2022-02-08 20:57:41 +0000

    AM: Refactorings

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,26 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations';
+import { Component, OnInit } from "@angular/core";
+import { trigger, transition, style, animate } from "@angular/animations";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css'],
-  animations: [ // Slide items up from the bottom of screen.
-        trigger('itemState', [
-            transition('void => *', [
-                style({transform: 'translateX(100%)'}),
-                animate('0.6s ease-in-out')
-            ]),
-            transition('* => void', [
-                animate('0.6s ease-in-out', style({transform: 'translateX(100%)'}))
-            ])
-        ])
-    ]
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
+  animations: [
+    // Slide items up from the bottom of screen.
+    trigger("itemState", [
+      transition("void => *", [
+        style({ transform: "translateX(100%)" }),
+        animate("0.6s ease-in-out"),
+      ]),
+      transition("* => void", [
+        animate("0.6s ease-in-out", style({ transform: "translateX(100%)" })),
+      ]),
+    ]),
+  ],
 })
 export class AboutComponent implements OnInit {
+  tabDetails = [
+    {
+      id: 1,
+      label: "University",
+      listItems: [
+        "Attended National University of Ireland, Galway",
+        "Honours degree in Bachelor of Commerce",
+        "Honours degree in h.Dip in Software Design & Development",
+      ],
+    },
+    {
+      id: 2,
+      label: "Career",
+      listItems: [
+        "Full-time I.T. professional based in Ireland",
+        "3 years experience developing web applications",
+      ],
+    },
+    {
+      id: 3,
+      label: "Portfolio",
+      listItems: [
+        "Collection of applications on my Github account.",
+        "Practiced advanced topics in Angular/Ionic, Typescript and the MEAN stack.",
+        "Honours degree in h.Dip in Software Design & Development",
+      ],
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
